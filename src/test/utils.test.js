eeec061bb95a7d476d067e4a25e2794ad0ee55a6
Andrew Oxenberg <adoxenberg@gmail.com>
2024-06-11 12:08:56 -0400

    feat: on push, merges branch with main branch if passes tests

--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -3,7 +3,15 @@ import { expect, test } from 'vitest';
 import { setDescription } from '../utils';
 
 test('testing setDescription', () => {
-    expect(setDescription("test.js")).toBe("Javascript");
+    expect(setDescription("test.js")).toBe("JavaScript");
     expect(setDescription("test.ts")).toBe("TypeScript");
     expect(setDescription("test.tsx")).toBe("test.tsx");
 });
+
+
+
+/*
+create new branch when adding a feature
+when you want to merge, yml file tests if your code works with tests before merging
+if successful, merges, otherwise doesnt.
+*/
