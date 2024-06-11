ae854c6cd6bbd0998902fbdd17bb39815f6a6af0
Andrew Oxenberg <adoxenberg@gmail.com>
2024-06-11 11:08:20 -0400

    workflow practice

--- vitest.config.js
+++ vitest.config.js
@@ -11,24 +11,3 @@ export default defineConfig({
 });
 
 
-// import { defineConfig, mergeConfig } from 'vitest/config';
-// import viteConfig from './vite.config';
-
-// export default mergeConfig(viteConfig, defineConfig({
-//   test: {
-//     include: ['src/test/**/*.test.{js,ts}'],
-//     exclude: ['out/**', '**/node_modules/**']
-//   }
-// }));
-
-// import { defineConfig } from 'vitest/config';
-
-// export default defineConfig({
-//   test: {
-//     // ...
-//   },
-// })
-
-//gitignore to ignore node_modules, out folder, 
-// config github workflow to run tests when commiting code to main branch
-//      c
